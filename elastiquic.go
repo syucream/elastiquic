c8c5518fd485b53b5f3261a9240f95eb9056ff17
Ryo Okubo <syucream1031@gmail.com>
2017-07-25 12:09:21 +0900

    Support JSON config and goroutine

--- elastiquic.go
+++ elastiquic.go
@@ -1,18 +1,66 @@
 package main
 
 import (
+  "encoding/json"
   "fmt"
+  "io/ioutil"
   "net/http"
+  "os"
 
   "github.com/devsisters/goquic"
 )
 
-func main() {
-  client := &http.Client{
-    Transport: goquic.NewRoundTripper(false),
-  }
-  resp, err := client.Get("https://www.google.co.jp/")
+const (
+  DEFINITIONS_FILE = "definitions.json"
+)
 
+type Definitions struct {
+  Scenarios []Scenario
+}
+
+type Scenario struct {
+  Url string
+  Expects Expects
+}
+
+type Expects struct {
+  Statuscode int
+}
+
+// Load json
+func load() Definitions {
+  file, err := ioutil.ReadFile(DEFINITIONS_FILE)
+  if err != nil {
+    fmt.Println(err)
+    // FIXME
+    os.Exit(1)
+  }
+
+  var defs Definitions
+  json.Unmarshal(file, &defs)
+
+  return defs
+}
+
+// Do QUIC request
+func request(client *http.Client, scenario Scenario, ch chan int) {
+  resp, err := client.Get(scenario.Url)
+
+  // For debug
   fmt.Println(resp)
   fmt.Println(err)
+
+  ch <- 1
+}
+
+func main() {
+  defs := load()
+
+  client := &http.Client {
+    Transport: goquic.NewRoundTripper(false),
+  }
+
+  ch := make(chan int)
+  go request(client, defs.Scenarios[0], ch)
+  <- ch
 }
